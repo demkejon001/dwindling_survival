50737c1eb4d5b52dc9f307124664b40433c1bfc2
jd-devel <102934622+jd-devel@users.noreply.github.com>
2024-10-25 17:31:49 -0400

    Test Commit

---
+++ Assets/Scripts/Misc/InteractableObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class InteractableObject : MonoBehaviour
+{
+    public string typeOfInteractable, interactableInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
