4eb261ceafe5e87c97f5831383d9a22bc8d4dece
jd-devel <102934622+jd-devel@users.noreply.github.com>
2024-10-25 20:00:17 -0400

    Import

--- Assets/Scripts/Misc/CollectableObject.cs
+++ Assets/Scripts/Misc/CollectableObject.cs
@@ -1,7 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 public class CollectableObject : MonoBehaviour
 {
-    public string itemID;
+    public int hasAmount;
+
+    private void Start()
+    {
+        hasAmount = 0;
+        objectCountNum.text = hasAmount.ToString();
+    }
+
+    public enum ItemID
+    {
+        Berry,
+        Wood,
+        Water,
+        Rock
+    }
+
+    public ItemID id = ItemID.Berry;
+
+    public TextMeshProUGUI objectCountNum;
+
+    public void Collect()
+    {
+        hasAmount++;
+        objectCountNum.text = hasAmount.ToString();
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -9,10 +10,23 @@ public class Player : MonoBehaviour
     public float maxWarmth = 100.0f;
     public float warmth = 100.0f;
     public float warmthIncrement = 1.0f;
-    // Start is called before the first frame update
+    public int[] inventory = new int[Enum.GetValues(typeof(CollectableObject)).Length];
+    public TextMeshProUGUI num;
 
     public void WarmUp()
     {
         warmth = math.clamp(warmth + warmthIncrement, 0, maxWarmth);
+    }
+
+    public void AddObject(CollectableObject objectToAdd)
+    {
+        int index = (int)objectToAdd.id;
+        inventory[index] += 1;
+        num.text = inventory[index].ToString();
+    }
+
+    private void Start()
+    {
+        Array.Fill(inventory, 0);
     }
 }
