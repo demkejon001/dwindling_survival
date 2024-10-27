211e3608c5422226326812c9bbf08f425ae798b6
demkejon001 <demkejon001@gmail.com>
2024-10-27 09:44:34 -0600

    adding sos sign

--- Assets/Scripts/SOS/SOS.cs
+++ Assets/Scripts/SOS/SOS.cs
@@ -6,8 +6,15 @@ public class SOS : MonoBehaviour
 {
     private int rocksNeeded; // Total rocks needed for the SOS sign
     private int rocksPlaced = 0; // Track how many rocks have been placed
-    public Sprite[] sosStages; // Array of sprites for each stage of the SOS sign
+    // public Sprite[] sosStages; // Array of sprites for each stage of the SOS sign
     public bool isFinished = false;
+    private SpriteRenderer[] spriteRenderers;
+
+    void Start()
+    {
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        rocksNeeded = spriteRenderers.Length;
+    }
 
     public void AddRock()
     {
@@ -18,16 +25,14 @@ public class SOS : MonoBehaviour
         }
     }
 
-    void Start()
-    {
-        rocksNeeded = sosStages.Length;
-    }
-
     private void UpdateSOS()
     {
         if (rocksPlaced <= rocksNeeded)
         {
-            GetComponent<SpriteRenderer>().sprite = sosStages[rocksPlaced]; // Update the sprite
+            // GetComponent<SpriteRenderer>().sprite = sosStages[rocksPlaced]; // Update the sprite
+            Color color = spriteRenderers[rocksPlaced-1].color;
+            color.a = 1;
+            spriteRenderers[rocksPlaced-1].color = color;
         }
         if (rocksPlaced == rocksNeeded)
         {
